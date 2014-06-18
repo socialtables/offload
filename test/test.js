a1c285eac92f16f9289ef54e444fb4b3efdb04f6
Matthew Chase Whittemore <matthew@socialtables.com>
2014-06-18 01:05:26 -0400

    adding some pending crap tests

--- test/test.js
+++ test/test.js
@@ -20,24 +20,30 @@ function asyncTester(done, fn){
 	}
 }
 
-describe("GET /jobs", function(){
-	it("should return data", function(done){
-		req.get("/jobs").end(asyncTester(done, function(data){
-			data.body.should.have.property("md5");
-			var jobKeys = Object.keys(data.body);
-			for(var i=0; i<jobKeys.length; i++){
-				var jobKey = jobKeys[i];
-				data.body[jobKey].should.have.property("running");
-				data.body[jobKey].should.have.property("done");
-				data.body[jobKey].should.have.property("error");
-			}
-		}));
-	});
+describe("GET", function(){
+	describe("/jobs", function(){
+		it("should return data", function(done){
+			req.get("/jobs").end(asyncTester(done, function(data){
+				data.body.should.have.property("md5");
+				var jobKeys = Object.keys(data.body);
+				for(var i=0; i<jobKeys.length; i++){
+					var jobKey = jobKeys[i];
+					data.body[jobKey].should.have.property("running");
+					data.body[jobKey].should.have.property("done");
+					data.body[jobKey].should.have.property("error");
+				}
+			}));
+		});
 
-	it("should run permitGet", function(done){
-		req.get("/jobs").end(asyncTester(done, function(data){
-			data.headers.should.have.property("permit-process", "get");
-		}));
+		it("should run permitGet", function(done){
+			req.get("/jobs").end(asyncTester(done, function(data){
+				data.headers.should.have.property("permit-process", "get");
+			}));
+		});
+	});
+	describe("custom route", function(){
+		it("should run as desired");
+		it("should still chain multiple middleware");
 	});
 });
 
@@ -58,34 +64,34 @@ describe("POST to", function(){
 			}));
 		});
 
-	});
-
-	describe("an invalid job", function(){
-		it.skip("whose command cannot run", function(done){
-			// this test is currently being skipped because I've yet to find a good way
-			// to capture the ENOENT error thrown by spawn when calling unknown files
-			req.post("/jobs/cla-not-installed").end(asyncTester(done, function(data){
-				console.log(data.statusCode, data.text);
+		it("should get 500 on a job fail", function(done){
+			req.post("/jobs/bad-exit").end(asyncTester(done, function(data){
+				data.statusCode.should.equal(500);
+				data.text.should.equal("Job Error");
 			}));
 		});
 
+		it("should run permitPost", function(done){
+			req.post("/jobs/good-exit").end(asyncTester(done, function(data){
+				data.headers.should.have.property("permit-process", "post");
+			}));
+		});
+
+		describe("that uses callback", function(){
+			it("should respond as expected if a real callback");
+		});
+
+		describe("that uses generator function", function(){
+			it("should respond as expected if a real generator function is supplied");
+		});
+
+	});
+
+	describe("an invalid job", function(){
 		it("that is not assigned", function(done){
 			req.post("/jobs/not-assigned").end(asyncTester(done, function(data){
 				data.statusCode.should.equal(404);
 			}));
 		});
-	});
-
-	it("should get 500 on a job fail", function(done){
-		req.post("/jobs/bad-exit").end(asyncTester(done, function(data){
-			data.statusCode.should.equal(500);
-			data.text.should.equal("Job Error");
-		}));
-	});
-
-	it("should run permitPost", function(done){
-		req.post("/jobs/good-exit").end(asyncTester(done, function(data){
-			data.headers.should.have.property("permit-process", "post");
-		}));
 	});
 });
